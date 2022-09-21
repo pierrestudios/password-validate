35f07f26b4cf81860f2fa37fdf01f4ad70c94147
pierrestudios <info@pierrestudios.com>
2022-09-21 08:11:26 -0400

    Clean integration tests

--- src/__tests__/PasswordField.test.tsx
+++ src/__tests__/PasswordField.test.tsx
@@ -2,15 +2,21 @@ import * as React from "react";
 import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 
-import PasswordField from "..";
+import { PasswordField } from "..";
 
 describe("Test PasswordField component for validator integration", () => {
-  test("Check for passed label in fields", () => {
-    const args = {
-      label: "Type your password",
-      label_confirm: "Confirm your password",
-    };
+  const mockChange = jest.fn();
+  const args = {
+    label: "Type your password",
+    label_confirm: "Confirm your password",
+    autorun: false,
+    onValidate: mockChange,
+  };
+  const test_password_bad = "abc";
+  const test_password_bad_caps = "ABC";
+  const test_password_bad_good = "abcdF@1";
 
+  test("Check for passed labels params in fields", () => {
     render(<PasswordField {...args} />);
 
     const passwordLabel = screen.getByText(args.label);
@@ -32,82 +38,68 @@ describe("Test PasswordField component for validator integration", () => {
     expect(passwordConfirmInput).toBeInTheDocument();
   });
 
-  test("Check for typed password in fields", async () => {
-    const args = {
-      label: "Type your password",
-      label_confirm: "Confirm your password",
-    };
-    const test_password = "abc";
-
+  test("Check for password typed in fields", async () => {
     render(<PasswordField {...args} />);
 
-    await userEvent.type(screen.getByTestId("test-password"), test_password);
+    await userEvent.type(
+      screen.getByTestId("test-password"),
+      test_password_bad
+    );
 
-    expect(screen.getByTestId("test-password")).toHaveValue(test_password);
+    expect(screen.getByTestId("test-password")).toHaveValue(test_password_bad);
 
     await userEvent.type(
       screen.getByTestId("test-password-confirm"),
-      test_password
+      test_password_bad
     );
 
     expect(screen.getByTestId("test-password-confirm")).toHaveValue(
-      test_password
+      test_password_bad
     );
   });
 
   test("Check for passed params, autorun, onValidate in fields", async () => {
-    const mockChange = jest.fn();
-    const args = {
-      label: "Type your password",
-      label_confirm: "Confirm your password",
-      autorun: true,
-      onValidate: mockChange,
-    };
-    const test_password = "abc";
+    const newArgs = { ...args };
+    newArgs.autorun = true;
 
-    render(<PasswordField {...args} />);
+    render(<PasswordField {...newArgs} />);
 
-    await userEvent.type(screen.getByTestId("test-password"), test_password);
+    await userEvent.type(
+      screen.getByTestId("test-password"),
+      test_password_bad
+    );
 
     // Note: autorun should cause errors to appear
     expect(
       screen.getByText(/Please check your password entry/i)
     ).toBeInTheDocument();
     expect(mockChange).toHaveBeenCalled();
-    expect(mockChange).toBeCalledWith(false, test_password);
+    expect(mockChange).toBeCalledWith(false, test_password_bad);
   });
 
   test("Check that Validate button works", async () => {
-    const mockChange = jest.fn();
-    const args = {
-      label: "Type your password",
-      label_confirm: "Confirm your password",
-      onValidate: mockChange,
-    };
-    const test_password = "abc";
-
     render(<PasswordField {...args} />);
 
-    await userEvent.type(screen.getByTestId("test-password"), test_password);
+    await userEvent.type(
+      screen.getByTestId("test-password"),
+      test_password_bad
+    );
     await userEvent.click(screen.getByText("Validate"));
 
     expect(
       screen.getByText(/Please check your password entry/i)
     ).toBeInTheDocument();
     expect(mockChange).toHaveBeenCalled();
-    expect(mockChange).toBeCalledWith(false, test_password);
+    expect(mockChange).toBeCalledWith(false, test_password_bad);
   });
 
   test("Check that all validation error displays and clears", async () => {
-    const args = {
-      label: "Type your password",
-      label_confirm: "Confirm your password",
-    };
-    const test_password = "abc";
-
     render(<PasswordField {...args} />);
 
-    await userEvent.type(screen.getByTestId("test-password"), test_password);
+    await userEvent.type(
+      screen.getByTestId("test-password"),
+      test_password_bad
+    );
     await userEvent.click(screen.getByText("Validate"));
 
     expect(screen.getByText(/6 or more characters/i)).toBeInTheDocument();
@@ -124,26 +116,24 @@ describe("Test PasswordField component for validator integration", () => {
     expect(screen.getByText(/To match password confirm/i)).toBeInTheDocument();
 
     // Test lowercase char
-    const test_password_caps = "ABC";
     await userEvent.clear(screen.getByTestId("test-password"));
     await userEvent.type(
       screen.getByTestId("test-password"),
-      test_password_caps
+      test_password_bad_caps
     );
     await userEvent.click(screen.getByText("Validate"));
 
     expect(screen.getByText(/At least 1 lowercase char/i)).toBeInTheDocument();
 
     // Clear all errors
-    const test_password_good = "abcdF@1";
     await userEvent.clear(screen.getByTestId("test-password"));
     await userEvent.type(
       screen.getByTestId("test-password"),
-      test_password_good
+      test_password_bad_good
     );
     await userEvent.type(
       screen.getByTestId("test-password-confirm"),
-      test_password_good
+      test_password_bad_good
     );
     await userEvent.click(screen.getByText("Validate"));
 
